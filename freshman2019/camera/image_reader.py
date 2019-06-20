4402d73e9a2e4411822cd9a4fcef836cd050019d
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-06-20 16:11:32 +0900

    add ImageReader, FileImageReader
    add CameraImageReader
    
    fix
    
    
    add comments

---
+++ freshman2019/camera/image_reader.py
@@ -0,0 +1,47 @@
+from abc import ABCMeta, abstractmethod
+import cv2
+import os
+from .color_image import ColorImage
+
+
+class ImageReader(metaclass=ABCMeta):  # abstract class
+    """
+    ColorImageをreadするもの.
+
+    SubClasses
+    ----------
+    FileImageReader
+    CameraImageReader
+    """
+
+    def read(self) -> ColorImage:
+        raise NotImplementedError
+
+
+class FileImageReader(ImageReader):
+    """
+    ファイルからColorImageをreadするもの.
+    """
+    filePath: str
+
+    def __init__(self, filePath: str):
+        assert os.path.isfile(filePath), "ファイル "+filePath+" は存在しません"
+        self.filePath = filePath
+
+    def read(self) -> ColorImage:
+        data = cv2.imread(self.filePath)
+        return ColorImage(data)
+
+
+class CameraImageReader(ImageReader):
+    """
+    カメラデバイスからColorImageをreadするもの.
+    """
+    capture: cv2.VideoCapture
+
+    def __init__(self, deviceNumber: int):
+        self.capture = cv2.VideoCapture(deviceNumber)
+
+    def read(self) -> ColorImage:
+        ret, frame = self.capture.read()
+        return ColorImage(frame)
