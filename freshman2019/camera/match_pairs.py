2d6826f7fbadd670b511199efc7ae1abe5a0ed80
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-07-02 19:32:41 +0900

    knnじゃないMatch

--- freshman2019/camera/match_pairs.py
+++ freshman2019/camera/match_pairs.py
@@ -10,7 +10,28 @@ class MatchPairs(object):
         特徴点マッチ結果
     """
 
-    pass
+    feature1: Feature
+    feature2: Feature
+    matches: List[cv2.DMatch]
+
+    def __init__(self, feature1: Feature, feature2: Feature, matches: List[cv2.DMatch],):
+        self.feature1 = feature1
+        self.feature2 = feature2
+        self.matches = matches
+
+    def plot(self) -> Image:
+        img1 = self.feature1.img
+        kp1 = self.feature1.kp
+        img2 = self.feature2.img
+        kp2 = self.feature2.kp
+        newImg = copy.copy(img2)
+        if newImg.nChannel() == 1:
+            newImg.data = cv2.cvtColor(newImg.data, cv2.COLOR_GRAY2BGR)
+        newImg.data = cv2.drawMatches(img1.data, kp1,
+                                      img2.data, kp2,
+                                      self.matches,
+                                      None, flags=2)
+        return newImg
 
 
 class KnnMatchPairs(object):
--- freshman2019/camera/matcher.py
+++ freshman2019/camera/matcher.py
@@ -1,6 +1,6 @@
 from typing import List
 from .feature import Feature
-from .match_pairs import KnnMatchPairs
+from .match_pairs import MatchPairs, KnnMatchPairs
 
 
 class Matcher(object):
@@ -10,6 +10,10 @@ class Matcher(object):
 
     def __init__(self, matcher):
         self.matcher = matcher
+
+    def match(self, feature1: Feature, feature2: Feature) -> MatchPairs:
+        matches = self.matcher.match(feature1.des, feature2.des)
+        return MatchPairs(feature1, feature2, matches)
 
     def knnMatch(self, feature1: Feature, feature2: Feature, k: int) -> KnnMatchPairs:
         matches = self.matcher.knnMatch(feature1.des, feature2.des, k)
