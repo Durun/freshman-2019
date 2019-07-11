beb34a90b1ac92e7b163531f7a47eaad11ce8644
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-07-11 20:10:38 +0900

    URLから画像を表示するテスト

---
+++ freshman2019/test/read_url_image_test.py
@@ -0,0 +1,12 @@
+import cv2
+from freshman2019.camera.color_image import ColorImage
+from freshman2019.camera.image_reader import UrlImageReader
+
+reader = UrlImageReader(
+    "https://gyazo.com/009c2f102ebb46ce326c4d153d5cf44a/thumb/1000")
+image = reader.read()
+image.show("image")
+
+print("Press any key on window to end.")
+cv2.waitKey(0)
+cv2.destroyAllWindows()
