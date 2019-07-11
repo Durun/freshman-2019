3f7b037be5b06fad5286bbc0d9cc91885ebc475f
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-07-11 20:42:36 +0900

    Cameraモジュールが訓練画像をURLから取得するように変更

--- freshman2019/camera/camera.py
+++ freshman2019/camera/camera.py
@@ -1,13 +1,13 @@
-from .image_reader import ImageReader, FileImageReader, CameraImageReader
+from .image_reader import ImageReader, UrlImageReader, CameraImageReader
 
 
 class Camera(object):
     queryReader: ImageReader
     trainReader: ImageReader
 
-    def __init__(self, deviceNumber: int, trainImagePath: str):
+    def __init__(self, deviceNumber: int, trainImageUrl: str):
         self.queryReader = CameraImageReader(deviceNumber)
-        self.trainReader = FileImageReader(trainImagePath)
+        self.trainReader = UrlImageReader(trainImageUrl)
 
     def get_temperature(self) -> int:
         raise NotImplementedError
