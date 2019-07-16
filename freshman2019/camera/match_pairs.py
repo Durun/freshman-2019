d10a41604d5e9c6fff34dcb3d6f80644ee74af3a
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-07-16 19:43:12 +0900

    KNNマッチを廃止

--- freshman2019/camera/match_pairs.py
+++ freshman2019/camera/match_pairs.py
@@ -116,30 +116,3 @@ class MatchPairs(object):
         distance(特徴空間上の距離)のリストへ変換する
         """
         return [m.distance for m in self.matches]
-
-
-class KnnMatchPairs(object):
-    feature1: Feature
-    feature2: Feature
-    matches: List[List[cv2.DMatch]]
-    k: int
-
-    def __init__(self, feature1: Feature, feature2: Feature, matches: List[List[cv2.DMatch]], k: int):
-        self.feature1 = feature1
-        self.feature2 = feature2
-        self.matches = matches
-        self.k = k
-
-    def plot(self) -> Image:
-        img1 = self.feature1.img
-        kp1 = self.feature1.kp
-        img2 = self.feature2.img
-        kp2 = self.feature2.kp
-        newImg = img2.copy()
-        if newImg.nChannel() == 1:
-            newImg.data = cv2.cvtColor(newImg.data, cv2.COLOR_GRAY2BGR)
-        newImg.data = cv2.drawMatchesKnn(img1.data, kp1,
-                                         img2.data, kp2,
-                                         self.matches,
-                                         None, flags=self.k)
-        return newImg
--- freshman2019/camera/matcher.py
+++ freshman2019/camera/matcher.py
@@ -1,6 +1,6 @@
 from typing import List
 from .feature import Feature
-from .match_pairs import MatchPairs, KnnMatchPairs
+from .match_pairs import MatchPairs
 
 
 class Matcher(object):
@@ -14,7 +14,3 @@ class Matcher(object):
     def match(self, feature1: Feature, feature2: Feature) -> MatchPairs:
         matches = self.matcher.match(feature1.des, feature2.des)
         return MatchPairs(feature1, feature2, matches)
-
-    def knnMatch(self, feature1: Feature, feature2: Feature, k: int) -> KnnMatchPairs:
-        matches = self.matcher.knnMatch(feature1.des, feature2.des, k)
-        return KnnMatchPairs(feature1, feature2, matches, k)
