d1689fe108779a0f95d024237fb2ddcb79a241f7
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-07-17 15:52:57 +0900

    matcher, detectorを統合

--- freshman2019/camera/match_algorithms.py
+++ freshman2019/camera/match_algorithms.py
@@ -1,18 +1,8 @@
 import cv2
-from .detector import Detector
 from .matcher import Matcher
 
 
-class MatchAlgorithm(object):
-    detector: Detector
-    matcher: Matcher
-
-    def __init__(self, detector: Detector, matcher: Matcher):
-        self.detector = detector
-        self.matcher = matcher
-
-
-ORB = MatchAlgorithm(
-    detector=Detector(cv2.ORB_create()),
-    matcher=Matcher(cv2.BFMatcher(cv2.NORM_HAMMING))
+ORB = Matcher(
+    detectorAlgorithm=cv2.ORB_create(),
+    matcherAlgorithm=cv2.BFMatcher(cv2.NORM_HAMMING)
 )
--- freshman2019/camera/matcher.py
+++ freshman2019/camera/matcher.py
@@ -1,17 +1,26 @@
 import cv2
 from typing import List
+from .image import Image
 from .feature import Feature
 from .match_pairs import MatchPairs
 
 
 class Matcher(object):
     """
-    特徴点Matcher
+    特徴点DetectorとMatcher
     """
+    detectorAlgorithm: cv2.Feature2D
     matcherAlgorithm: cv2.DescriptorMatcher
 
-    def __init__(self, matcherAlgorithm: cv2.DescriptorMatcher):
+    def __init__(self,
+                 detectorAlgorithm: cv2.Feature2D,
+                 matcherAlgorithm: cv2.DescriptorMatcher):
+        self.detectorAlgorithm = detectorAlgorithm
         self.matcherAlgorithm = matcherAlgorithm
+
+    def detect(self, image: Image) -> Feature:
+        kp, des = self.detectorAlgorithm.detectAndCompute(image.data, None)
+        return Feature(image, kp, des)
 
     def match(self, feature1: Feature, feature2: Feature) -> MatchPairs:
         matches = feature1.match(feature2, self.matcherAlgorithm)
