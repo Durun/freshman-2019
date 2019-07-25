4d7cfe681b0e57ae07435ba0e7adcab27e6c9057
ando-n <ando.n.ad@se.cs.titech.ac.jp>
2019-07-25 18:19:43 +0900

    get_temperature の実装

--- freshman2019/camera/camera.py
+++ freshman2019/camera/camera.py
@@ -1,21 +1,35 @@
 from .image import ImageReader, UrlImageReader, CameraImageReader
 from .image import Image
 from .panel_trimmer import PanelTrimmer
+from .ocr import Recognizer
+from .recognition_error import RecognitionError
+import pyocr
+import logging
 
 
 class Camera(object):
     queryReader: ImageReader
     trainReader: ImageReader
     trimmer: PanelTrimmer
+    recognizer: Recognizer
 
     def __init__(self, deviceNumber: int, trainImageUrl: str):
         self.queryReader = CameraImageReader(deviceNumber)
         self.trainReader = UrlImageReader(trainImageUrl)
         trainImg = self.trainReader.read()
         self.trimmer = PanelTrimmer(trainImg)
+        self.recognizer = Recognizer(
+            tool=pyocr.tesseract, lang="letsgodigital")
 
     def get_temperature(self) -> int:
-        raise NotImplementedError
+        tempImg = self.getTemperetureImage()
+        tempText = self.recognizer.imageToText(tempImg)
+        try:
+            tempInt = int(tempText)
+        except ValueError:
+            logging.warn("got " + tempText)
+            raise RecognitionError("got " + tempText)
+        return tempInt
 
     def is_power_on(self) -> bool:
         raise NotImplementedError
